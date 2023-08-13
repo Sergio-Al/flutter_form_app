ba6eb457397ef7e9f75db6925ebee0459977d44d
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-08-13 12:52:33 -0400

    Working with request & waiting for the response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -23,21 +23,35 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('flutter-test-3506b-default-rtdb.firebaseio.com',
           'shopping-list.json');
       print(url);
-      http.post(url,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: json.encode({
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
             'name': _enteredName,
             'quantity': _enteredQuantity,
             'category': _selectedCategory.name,
-          }));
+          },
+        ),
+      );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        // if the context is not mounted we'll return
+        return;
+      }
+
+      Navigator.of(context).pop();
 
       // Navigator.of(context).pop(
       //   GroceryItem(
